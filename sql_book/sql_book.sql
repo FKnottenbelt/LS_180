d52f2570001ef8547d76814fe70b6e08d481facc
Femke Knottenbelt <femknotgithub@gmail.com>
2019-02-14 13:01:37 +0000

    Start alter table examples

--- sql_book/sql_book.sql
+++ sql_book/sql_book.sql
@@ -1,5 +1,20 @@
 CREATE TABLE users (
   id serial UNIQUE NOT NULL,
-  username CHAR(25),
+  username CHAR(25) NOT NULL,
   enabled boolean DEFAULT TRUE
   );
+
+-- Renaming a Table
+ALTER TABLE users RENAME TO all_users;
+
+-- Renaming a Column
+ALTER TABLE all_users RENAME COLUMN username TO full_name;
+
+-- Changing a Column's Datatype
+ALTER TABLE all_users ALTER COLUMN full_name TYPE VARCHAR(25);
+
+-- Removing a column Constraint (removing sequence)
+ALTER TABLE all_users ALTER COLUMN id DROP DEFAULT;
+
+-- alter table test alter column id drop not null;
+
