a6717acddc6dda6660a67c1b7ccdfaecf93c320b
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-02 11:58:33 +0000

    Finish more single table exercises

--- 02_schema_data_sql/more_single_table.sql
+++ 02_schema_data_sql/more_single_table.sql
@@ -10,3 +10,27 @@ SELECT state, COUNT(id)
 FROM people
 GROUP BY state
 ORDER BY count DESC LIMIT 10;
+
+
+select distinct substring(email,'@.+') as domain,
+       count(id)
+  from people
+  group by domain
+  order by count desc;
+
+-- or
+
+SELECT substr(email, strpos(email, '@') + 1) as domain,
+       COUNT(id)
+  FROM people
+  GROUP BY domain
+  ORDER BY count DESC;
+
+DELETE FROM people WHERE id = 3399;
+
+DELETE FROM people WHERE state = 'CA';
+
+UPDATE people SET given_name = UPPER(given_name)
+ WHERE email LIKE '%teleworm.us';
+
+DELETE FROM people;
