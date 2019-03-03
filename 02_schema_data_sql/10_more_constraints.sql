66edd1e1682ce1bfbdf4a90e1f84d1b09bdb8aca
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-03 11:26:47 +0000

    Add more constraints exercises 1 to 7

---
+++ 02_schema_data_sql/10_more_constraints.sql
@@ -0,0 +1,16 @@
+\i films2.sql
+
+ALTER TABLE films ALTER COLUMN title SET NOT NULL;
+ALTER TABLE films ALTER COLUMN year SET NOT NULL;
+ALTER TABLE films ALTER COLUMN genre SET NOT NULL;
+ALTER TABLE films ALTER COLUMN director SET NOT NULL;
+ALTER TABLE films ALTER COLUMN duration SET NOT NULL;
+
+ALTER TABLE films ADD CONSTRAINT title_unique UNIQUE (title);
+
+ALTER TABLE films DROP CONSTRAINT title_unique;
+
+ALTER TABLE films
+  ADD CONSTRAINT title_length
+  CHECK (length(title) >= 1);
+
