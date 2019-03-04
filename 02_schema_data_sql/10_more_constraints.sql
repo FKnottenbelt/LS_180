5e33a12e35aa9f13380ad20f3e4533c1438d66e9
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-04 09:51:31 +0000

    Finish constraint exercises

--- 02_schema_data_sql/10_more_constraints.sql
+++ 02_schema_data_sql/10_more_constraints.sql
@@ -14,3 +14,28 @@ ALTER TABLE films
   ADD CONSTRAINT title_length
   CHECK (length(title) >= 1);
 
+INSERT INTO films VALUES ('', 1901, 'action', 'JJ Abrams', 126);
+
+ALTER TABLE films DROP CONSTRAINT title_length;
+
+ALTER TABLE films
+ADD CONSTRAINT year_range CHECK (year BETWEEN 1900 AND 2100);
+
+-- ALTER TABLE films ADD CONSTRAINT director_len
+-- CHECK (length(director) >= 3 AND
+--       strpos(director,' ') > 0);
+
+ALTER TABLE films ADD CONSTRAINT director_name
+    CHECK (length(director) >= 3
+      AND position(' ' in director) > 0);
+
+
+SELECT * FROM films WHERE strpos(title, 'ard') <> 0;
+
+UPDATE films SET director = 'Johnny' WHERE title = 'Die Hard';
+
+CREATE TABLE shoes (name text,
+                    size numeric(3,1) DEFAULT 0);
+
+ALTER TABLE shoes ADD CONSTRAINT shoe_size
+  CHECK (size BETWEEN 1 AND 15);
