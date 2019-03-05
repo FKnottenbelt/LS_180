b5f963766367d64a5f4b4b49401410c6da7ae199
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-05 19:14:36 +0000

    Add group by exercises 7 to 11

--- 02_schema_data_sql/12_group_by_and_aggregate.sql
+++ 02_schema_data_sql/12_group_by_and_aggregate.sql
@@ -14,3 +14,28 @@ SELECT genre,
   FROM films
   GROUP BY genre;
 
+SELECT year / 10 * 10 as decade,
+ ROUND(AVG(duration)) as average_duration
+  FROM films
+  GROUP BY decade
+  ORDER BY decade;
+
+SELECT * FROM films WHERE director LIKE 'John %';
+
+SELECT genre, count(id)
+  FROM films
+  GROUP BY genre
+  ORDER BY count DESC;
+
+SELECT year / 10 * 10 AS decade,
+       genre,
+       string_agg(title, ', ') AS films
+  FROM films
+  GROUP BY decade, genre
+  ORDER BY decade, genre;
+
+SELECT genre,
+       sum(duration) AS total_duration
+  FROM films
+  GROUP BY genre
+  ORDER BY total_duration ASC;
