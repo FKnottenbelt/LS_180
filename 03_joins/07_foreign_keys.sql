d4f55b7fdfe8cf2321a1c92f4bd52efdb858204c
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-09 13:57:55 +0000

    Add foreign keys exercises 8 to 10

--- 03_joins/07_foreign_keys.sql
+++ 03_joins/07_foreign_keys.sql
@@ -1,1 +1,41 @@
 \i orders_products1.sql
+
+ALTER TABLE orders
+ADD CONSTRAINT orders_product_id_fkey FOREIGN KEY (product_id)
+REFERENCES products(id);
+
+INSERT INTO products
+  VALUES(DEFAULT,'small bolt'),
+        (DEFAULT, 'large bolt');
+INSERT INTO orders (quantity, product_id)
+   VALUES (10, 1),
+          (25, 1),
+          (15, 2);
+
+SELECT orders.quantity, products.name
+  FROM orders
+  INNER JOIN products ON products.id = orders.product_id;
+
+INSERT INTO orders (quantity) VALUES (0);
+
+DELETE FROM orders WHERE product_id IS NULL;
+ALTER TABLE orders ALTER COLUMN product_id SET NOT NULL;
+
+-- CREATE TABLE reviews (
+--   id serial PRIMARY KEY,
+--   product_id INT NOT NULL,
+--   review text NOT NULL,
+--   FOREIGN KEY (product_id) REFERENCES products(id)
+-- );
+
+CREATE TABLE reviews (
+  id serial PRIMARY KEY,
+  body text NOT NULL,
+  product_id integer REFERENCES products (id)
+);
+
+INSERT INTO reviews (product_id, body)
+  VALUES (1, 'a little small'),
+         (1, 'very round!'),
+         (2, 'could have been smaller');
+
