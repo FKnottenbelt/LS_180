8a096856ff5896a5917b5b94131f2e942f115cad
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-09 09:05:38 +0000

    Add multiple table exercises 6 and 7

--- 03_joins/06_mutilple_tables.sql
+++ 03_joins/06_mutilple_tables.sql
@@ -13,3 +13,21 @@ SELECT events.name, count(tickets.id) AS popularity
   JOIN tickets ON events.id = tickets.event_id
   GROUP BY events.name
   ORDER BY popularity DESC;
+
+SELECT customers.id, customers.email,
+       COUNT(DISTINCT tickets.event_id)
+  FROM customers INNER JOIN tickets on tickets.customer_id = customers.id
+  GROUP BY customers.id
+  HAVING COUNT(DISTINCT tickets.event_id) = 3;
+
+SELECT events.name AS event,
+       events.starts_at,
+       sections.name AS section, s
+       eats.row,
+       seats.number AS seat
+  FROM tickets
+    INNER JOIN events ON tickets.event_id = events.id
+    INNER JOIN customers ON tickets.customer_id = customers.id
+    INNER JOIN seats ON tickets.seat_id = seats.id
+    INNER JOIN sections ON seats.section_id = sections.id
+WHERE customers.email = 'gennaro.rath@mcdermott.co';
