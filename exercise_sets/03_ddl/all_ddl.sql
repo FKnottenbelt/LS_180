a9fbc562d15b512fe2facf2927819e4a91dbe86e
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-12 18:40:22 +0000

    Add ddl exercise no 9

--- exercise_sets/03_ddl/all_ddl.sql
+++ exercise_sets/03_ddl/all_ddl.sql
@@ -123,3 +123,10 @@ ALTER TABLE planets ADD COLUMN semi_major_axis numeric NOT NULL;
 -- ADD COLUMN semi_major_axis numeric NOT NULL;
 
 -- 09
+CREATE TABLE moons (
+  id serial PRIMARY KEY,
+  designation integer NOT NULL CHECK (designation > 0),
+  semi_major_axis numeric CHECK (semi_major_axis > 0.0),
+  mass numeric CHECK (mass > 0.0),
+  planet_id integer NOT NULL REFERENCES planets (id)
+);
