40a375f81981a5655d61b775cd8da971982543ec
Femke Knottenbelt <femknotgithub@gmail.com>
2019-03-13 18:23:58 +0000

    Add DML exercise no 6

--- exercise_sets/04_dml/all_ddl.sql
+++ exercise_sets/04_dml/all_ddl.sql
@@ -48,3 +48,11 @@ SELECT devices.name, COUNT(parts.part_number)
 FROM devices
 INNER JOIN parts on parts.device_id = devices.id
 GROUP BY devices.name;
+
+-- 06
+
+SELECT devices.name AS name, COUNT(parts.device_id)
+FROM devices
+JOIN parts ON devices.id = parts.device_id
+GROUP BY devices.name
+ORDER BY devices.name;
