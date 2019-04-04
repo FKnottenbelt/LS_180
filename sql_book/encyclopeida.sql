3bc1cbfd14785c0300c53d519d986c90d642bfcf
Femke Knottenbelt <femknotgithub@gmail.com>
2019-04-04 18:14:16 +0000

    Redo encyclopedia table relationship exercises

--- sql_book/encyclopeida.sql
+++ sql_book/encyclopeida.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS encyclopedia;
+
+CREATE DATABASE encyclopedia;
+
+\c encyclopedia
+
 CREATE TABLE countries (
   id serial,
   name varchar(50) UNIQUE NOT NULL,
@@ -160,7 +166,7 @@ INSERT INTO continents (continent_name) VALUES
 INSERT INTO countries (name, capital, population, continent_id)
   VALUES  ('Brazil', 'Brasilia', 208385000, 5),
           ('Egypt', 'Cairo', 96308900, 1),
-          ('France', 'Paris', 67158000, 3),
+          ('France', 'Pais', 67158000, 3),
           ('Germany', 'Berlin', 82349400, 3),
           ('Japan', 'Tokyo', 126672000, 2),
           ('USA', 'Washington D.C.', 325365189, 4);
